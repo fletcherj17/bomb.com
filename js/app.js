9a6766a4d2f7f8fe9a7708902002fc8c56a735a7
fletcherj17 <fletcherejones@gmail.com>
2020-05-14 17:24:00 -0400

    Complete MVP

--- js/app.js
+++ js/app.js
@@ -33,13 +33,17 @@ let timeRemaining = clock.toString();
 const setTimer = () => {
     $('#start').hide();
     const timer = setInterval( ()=>{
-        if (time === 0) {
+        if (($('#game-1').css('background-color') === completedColor && $('#game-2').css('background-color') === completedColor && $('#game-3').css('background-color') === completedColor && $('#game-4').css('background-color') === completedColor) || $strikes.text() == "X X X "){
+            clearInterval(timer);
+        } else if (time === 0) {
             $('#countdown').text('0:00');
             clearInterval(timer);
-            if (confirm('BOMB EXPLODED')) {
-                location.reload(true);   
-            }
             $('#start').show();
+            if (confirm('BOMB EXPLODED! Press "OK" to try again.')) {
+                location.reload(true);   
+            } else {
+                $('.mini-game').off();
+            }
         } else if (time >= 60){
         timeRemaining = clock.toString().split('');
         timeRemaining.splice(1,0,':');
@@ -76,23 +80,26 @@ const setTimer = () => {
             timeRemaining.splice(1,1);
             timeRemaining = Number(timeRemaining.join(''));
             timeRemaining--; 
-            time--
+            time--;
         } else if (time < 10){
-            $('#countdown').css("animation", "blink .4s linear infinite")
+            $('#countdown').css("animation", "blink .4s linear infinite");
             $('#countdown').text(`0:0${time}`);
-            time--
+            time--;
         }
     }, 1000);
 };
 
 //Minigame Gameplay
 //Opening and closing mini-games
-let minigameColors = ["rgba(25,70,150,0.7)","rgba(50,35,70,0.7)","rgba(230,50,5,0.7)","rgba(188,131,0,0.7)"]
+let minigameColors = ["rgba(25, 70, 150, 0.7)","rgba(50, 35, 70, 0.7)","rgba(230, 50, 5, 0.7)","rgba(188, 131, 0, 0.7)"];
 $(".mini-game").on("click",function(event){
     if ($("#close-button").css('display') === "none"){
     $currentMiniGame = $(event.target);
-    $currentMiniGame.css('background-color', minigameColors[0])
-    minigameColors.splice(0,1)
+    console.log($currentMiniGame)
+    if ($currentMiniGame.css('background-color') === "rgba(0, 0, 0, 0)") {
+        $currentMiniGame.css('background-color', minigameColors[0]);
+        minigameColors.splice(0,1)
+    }
     $currentMiniGameParent = $currentMiniGame.parent();
     $currentMiniGameChild = $currentMiniGame.children();
     console.log($currentMiniGameChild);
@@ -111,18 +118,24 @@ $(".mini-game").on("click",function(event){
     $("#close-button").hide();
     });
 });
+
 // Receiving player responses and reacting
 let $strikes = $('#strike-count')
 let correctResponses = ['11','thursday','40','5'];
 let altCorrectResponses = ['eleven','Thursday','forty','five'];
 let moreCorrectResponses = ['Eleven','thurs','40 socks','five minutes'];
+let completedColor = 'rgba(12, 111, 1, 0.6)';
 $(".submit").on("click",function(event){
     $currentSubmitBtn = $(event.target);
     $currentResponse = $currentSubmitBtn.parents().eq(0).siblings().eq(2).val();
     if (!(correctResponses.includes($currentResponse) || moreCorrectResponses.includes($currentResponse) || altCorrectResponses.includes($currentResponse))){
         if ($strikes.text() == "X X ") {
-            if (confirm('BOMB EXPLODED')) {
+            $strikes.text($strikes.text() + "X ");
+            $('#start').show();
+            if (confirm('BOMB EXPLODED! Press "OK" to try again.')) {
                 location.reload(true);   
+            } else {
+                $('.mini-game').off();
             }
         } else if ($strikes.text() == "O STRIKES"){
         console.log('works');
@@ -136,10 +149,18 @@ $(".submit").on("click",function(event){
         return $strikes.text($strikes.text() + "X ");
         }
     } else {
-        $currentSubmitBtn.parents().eq(2).css('background-color', 'rgba(12,111,1,0.6)')
+        $currentSubmitBtn.parents().eq(2).css('background-color', completedColor)
         $currentSubmitBtn.parents().eq(0).siblings().eq(2).val("");
         $currentSubmitBtn.parents().eq(0).siblings().eq(0).text('BOMB SECTION DEFUSED!');
         $currentSubmitBtn.parents().eq(0).siblings().eq(1).css('text-decoration', "line-through");
+        if ($('#game-1').css('background-color') === completedColor && $('#game-2').css('background-color') === completedColor && $('#game-3').css('background-color') === completedColor && $('#game-4').css('background-color') === completedColor) {
+            $('#start').show();
+            if (confirm('BOMB DEFUSED! Press "OK" to play again.')) {
+                location.reload(true);   
+            } else {
+                $('.mini-game').off();
+            }
+        }
     }
 });
 
