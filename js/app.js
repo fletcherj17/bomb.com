8a80c38569295376ef88c265fc1138fe90f45ada
fletcherj17 <fletcherejones@gmail.com>
2020-05-14 21:24:22 -0400

    Finished major strech goal, LEADERBOARD

--- js/app.js
+++ js/app.js
@@ -36,8 +36,10 @@ const setTimer = () => {
             $('#countdown').text('0:00');
             clearInterval(timer);
             $('#start').show();
-            if (confirm('BOMB EXPLODED! Press "OK" to try again. Press "Cancel" to display the leaderboard.')) {
-                location.reload(true);   
+            if (confirm('BOMB EXPLODED! Press "OK" to try again.')) {
+                $('#close-button').click();
+                $('.mini-game').off();
+                $('aside').show();   
             } else {
                 $('#close-button').click();
                 $('.mini-game').off();
@@ -88,7 +90,40 @@ const setTimer = () => {
     }, 1000);
 };
 
-//Minigame Gameplay
+//Get leaderboard scores
+let savedScores = window.localStorage;
+savedScores.clear();
+let addFakeHighScores = () =>{
+savedScores.setItem('Fletcher', '80s');
+savedScores.setItem('Fletcher Sr.', '77s');
+savedScores.setItem('Fletcher\'s Mom', '51s');
+savedScores.setItem('Che', '51s');
+savedScores.setItem('Britt', '63s');
+savedScores.setItem('Debra', '91s');
+savedScores.setItem('Holden', '45s');
+savedScores.setItem('Brad', '69s');
+}
+let highScores = [];
+let getScores = () => {
+    addFakeHighScores();
+    for(var i=0; i < savedScores.length; i++){
+        let obj = {};
+        obj.name = savedScores.key(i);
+        obj.score = savedScores.getItem(savedScores.key(i));
+        highScores.push(obj)
+    }
+    highScores.sort((a, b) => (a.score > b.score) ? 1 : -1)
+    while (highScores.length > 7){
+        highScores.pop();
+    }
+    let leaderboard = '';
+    for(i=0; i<highScores.length; i++){
+        leaderboard += `<p>${highScores[i].name} — ${highScores[i].score}</p>`
+    }
+    return leaderboard;
+}
+
+/* Minigame Gameplay */
 //Opening and closing mini-games
 let minigameColors = ["rgba(25, 70, 150, 0.7)","rgba(50, 35, 70, 0.7)","rgba(230, 50, 5, 0.7)","rgba(200, 131, 0, 0.7)"];
 $(".mini-game").on("click",function(event){
@@ -107,15 +142,15 @@ $(".mini-game").on("click",function(event){
     $('input').focus();
     $("#close-button").show();
     }
-
     $("#close-button").on('click', function(event){
-    $currentMiniGameChild.hide();
-    $('.mini-game').show();
-    $($currentMiniGameParent).append($currentMiniGame);
-    $currentMiniGame.css('background-color', "none")
-    $("#close-button").hide();
-    });
+        $currentMiniGameChild.hide();
+        $('.mini-game').show();
+        $($currentMiniGameParent).append($currentMiniGame);
+        $currentMiniGame.css('background-color', "none")
+        $("#close-button").hide();
+        });
 });
+
 // Receiving player responses and reacting
 let $strikes = $('#strike-count')
 let correctResponses = ['11','thursday','40','5'];
@@ -129,8 +164,10 @@ $(".submit").on("click",function(event){
         if ($strikes.text() == "X X ") {
             $strikes.text($strikes.text() + "X ");
             $('#start').show();
-            if (confirm('BOMB EXPLODED! Press "OK" to try again. Press "Cancel" to display the leaderboard.')) {
-                location.reload(true);   
+            if (confirm('BOMB EXPLODED!')) {
+                $('#close-button').click();
+                $('.mini-game').off();
+                $('aside').show();  
             } else {
                 $('#close-button').click();
                 $('.mini-game').off();
@@ -139,7 +176,7 @@ $(".submit").on("click",function(event){
         } else if ($strikes.text() == "0 STRIKES"){
         return $strikes.text('X ');
         } else if ($strikes.text() == "X "){
-        $($strikes).css('color','red');
+        $strikes.css('color','red');
         $strikes.text($strikes.text() + "X ");
         $strikes.css("transform", "scale(2)")
         $strikes.css("transition", ".1s linear")
@@ -149,18 +186,26 @@ $(".submit").on("click",function(event){
     } else {
         $currentSubmitBtn.hide();
         $currentSubmitBtn.parents().eq(1).css('background-color', completedColor)
-        $currentSubmitBtn.siblings().eq(2).val("");
+        $currentSubmitBtn.siblings().eq(2).hide();
         $currentSubmitBtn.siblings().eq(0).text('BOMB SECTION DEFUSED!');
         $currentSubmitBtn.siblings().eq(1).css('text-decoration', "line-through");
         if ($('#game-1').css('background-color') === completedColor && $('#game-2').css('background-color') === completedColor && $('#game-3').css('background-color') === completedColor && $('#game-4').css('background-color') === completedColor) {
             $('#start').show();
-            if (confirm('BOMB DEFUSED! Press "OK" to play again. Press "Cancel" to display the leaderboard.')) {
-                location.reload(true);   
+            if (confirm('BOMB DEFUSED! Press "OK" to play again.')) {
+                $('#close-button').click();
+                $('.mini-game').off();
+                let name = prompt('Enter your name:')
+                let score = (120 - time) + "s"
+                savedScores.setItem(name, score);
+                $('.board-body').append(`${getScores()}`);
+                $('aside').show();  
             } else {
                 $('#close-button').click();
                 $('.mini-game').off();
                 let name = prompt('Enter your name:')
-                $('.board-body').append(`<p>${name} — ${time}s</p>`)
+                let score = (120 - time) + "s"
+                savedScores.setItem(name, score);
+                $('.board-body').append(`${getScores()}`);
                 $('aside').show();
             }
         }
@@ -171,6 +216,7 @@ $(".submit").on("click",function(event){
 
 //Start button
 $('#start').on('click', startGame);
+
 //Allows the enter key to submit answers
 $("input").keyup(function(event) {
     if (event.keyCode === 13) {
@@ -178,3 +224,5 @@ $("input").keyup(function(event) {
         $target.siblings(2).click();
     }
 });
+
+
